2d59cc1794614c64c404e4f10bd475012521b2d7
lidimi <lidsheq97@gmail.com>
2023-02-12 20:59:00 +0100

    homepage content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <div className="homepage">
+        <div className="homepage-content">
+          <div className="homepage-about">
+            <p className="homepage-about-travel">SO, YOU WANT TO TRAVEL TO</p>
+            <h1>SPACE</h1>
+            <p className="homepage-about-desc">
+              Let’s face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we’ll give you a truly out of
+              this world experience!
+            </p>
+          </div>
+          <div className="homepage-button">
+            <button className="homepage-btn">EXPLORE</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,7 @@
+import navIcon from "../assets/shared/logo.svg";
+
+function Navbar() {
+  return <div className="navbar"></div>;
+}
+
+export default Navbar;
