26ad682031b1c8713e0df5dda0bc08440fb00841
lidimi <lidsheq97@gmail.com>
2023-02-12 21:48:31 +0100

    navbar component styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="app">
       <div className="homepage">
+        <Navbar />
         <div className="homepage-content">
           <div className="homepage-about">
             <p className="homepage-about-travel">SO, YOU WANT TO TRAVEL TO</p>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,34 @@
 import navIcon from "../assets/shared/logo.svg";
 
 function Navbar() {
-  return <div className="navbar"></div>;
+  return (
+    <div className="navbar">
+      <div className="navbar-logo">
+        <img src={navIcon} alt="logo" />
+      </div>
+      <div className="navbar-decorative">
+        <div className="line"></div>
+      </div>
+      <div className="navbar-navigation">
+        <nav>
+          <ul className="desktop-nav">
+            <li>
+              <span className="navigation--number">00</span> HOME
+            </li>
+            <li>
+              <span className="navigation--number">01</span> DESTINATION
+            </li>
+            <li>
+              <span className="navigation--number">02</span> CREW
+            </li>
+            <li>
+              <span className="navigation--number">03</span> TECHNOLOGY
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
