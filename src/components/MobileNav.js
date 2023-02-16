d9911f0ec01d8e0fe85111fe223210f7479a071e
lidimi <lidsheq97@gmail.com>
2023-02-16 21:46:14 +0100

    mobile nav

---
+++ src/components/MobileNav.js
@@ -0,0 +1,9 @@
+function MobileNav({ links }) {
+  return (
+    <div className="mobile-links">
+      <ul className="mobile-links-list">{links}</ul>
+    </div>
+  );
+}
+
+export default MobileNav;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,35 @@
 import navIcon from "../assets/logo.svg";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import iconHamburger from "../assets/icon-hamburger.svg";
+import iconClose from "../assets/icon-close.svg";
+import MobileNav from "../components/MobileNav";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const links = [
+    { path: "/", number: "00", textContent: "home" },
+    { path: "/destination", number: "01", textContent: "destination" },
+    { path: "/crew", number: "02", textContent: "crew" },
+    { path: "/technology", number: "03", textContent: "technology" },
+  ];
+
+  const navLinks = links.map((link) => {
+    return (
+      <li key={link.number}>
+        <NavLink to={link.path}>
+          <span className="navigation--number">{link.number}</span>
+          {link.textContent}
+        </NavLink>
+      </li>
+    );
+  });
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="navbar">
       <div className="navbar-logo">
@@ -12,29 +40,14 @@ function Navbar() {
       </div>
       <div className="navbar-navigation">
         <nav>
-          <ul className="desktop-nav">
-            <li>
-              <NavLink to="/">
-                <span className="navigation--number">00</span> HOME
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/destination">
-                <span className="navigation--number">01</span> DESTINATION
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/crew">
-                <span className="navigation--number">02</span> CREW
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/technology">
-                <span className="navigation--number">03</span> TECHNOLOGY
-              </NavLink>
-            </li>
-          </ul>
+          <ul className="desktop-nav">{navLinks}</ul>
         </nav>
+      </div>
+      <div className="navbar-mobile-nav">
+        <button className="mobile-nav--button" onClick={handleClick}>
+          <img src={isOpen ? iconClose : iconHamburger} alt="" />
+        </button>
+        {isOpen && <MobileNav links={navLinks} />}
       </div>
     </div>
   );
